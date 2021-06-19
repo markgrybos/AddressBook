372246398098b52a91b81a57157b1198c48ee4c8
MisbaIram <63822686+MisbaIram@users.noreply.github.com>
2021-06-19 12:51:31 -0700

    Added a new oage AddPage.xml to add a new contact

--- AddressBook/AddPage.xaml.cs
+++ AddressBook/AddPage.xaml.cs
@@ -12,35 +12,33 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-//using System.Windows.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AddressBook
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class AddPage : Page
     {
-        public MainPage()
+        public AddPage()
         {
             this.InitializeComponent();
         }
 
-        private void ListOfLettersView_ItemClick(object sender, ItemClickEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void AddButton_Click(object sender, RoutedEventArgs e)
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            ContactsByLetters.IsPaneOpen = !ContactsByLetters.IsPaneOpen;
-            //clear the list of address
-            // this.NavigationService.Navigate(new Uri("/AddPage.xaml", UriKind.Relative));
-            this.Frame.Navigate(typeof(AddPage));
 
+        }
 
+        private void NewEmailId_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
         }
     }
